1869fdbb5f4a853cbcf0025368481ec8574cd007
Akagi201 <akagi201@gmail.com>
2023-09-05 11:43:49 +0800

    feat: update bolilerplate code

--- src/poetry_template/main.py
+++ src/poetry_template/main.py
@@ -1,15 +1,27 @@
 import argparse
+from argparse import Namespace
 import asyncio
+import yaml
 
 
-def parse_args():
-    parser = argparse.ArgumentParser()
+def parse_args() -> Namespace:
+    parser = argparse.ArgumentParser(description="Liquid Searcher")
+    parser.add_argument(
+        "--config",
+        "-c",
+        type=str,
+        required=True,
+        help="config file",
+    )
     args = parser.parse_args()
     return args
 
 
-async def main(args=None):
-    print("hello micro-py")
+async def main(args: Namespace):
+    print("parsing config: {args.config}")
+    with open(args.config, "r") as config_file:
+        config = yaml.safe_load(config_file)
+    print("config: {}".format(config))
 
 
 if __name__ == "__main__":
